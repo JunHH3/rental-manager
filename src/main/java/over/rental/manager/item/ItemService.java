17ffc120b23cf4f401f7bebd663502c79a75c30f
JunHH3 <tarakaing@naver.com>
2026-09-07 20:51:00 +0900

    day17 트랜잭션

--- src/main/java/over/rental/manager/item/ItemService.java
+++ src/main/java/over/rental/manager/item/ItemService.java
@@ -3,6 +3,7 @@ package over.rental.manager.item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ItemService {
@@ -13,6 +14,7 @@ public class ItemService {
         this.itemRepository = itemRepository;
     }
 
+    @Transactional(readOnly = true)
     public Page<Item> findItems(String keyword, Pageable pageable) {
         if (keyword.isBlank()) {
             return itemRepository.findAll(pageable);
@@ -21,35 +23,40 @@ public class ItemService {
     }
 
 
-
+    @Transactional
     public Item createItem(ItemForm form) {
         Item item = new Item(form.getName(), form.getDescription());
         itemRepository.save(item);
         return item;
     }
 
+    @Transactional(readOnly = true)
     public Item findItem(Long id) {
         return itemRepository.findById(id).orElseThrow(
                 () -> new ItemNotFoundException("물건을 찾을 수 없습니다.")
         );
     }
 
+    @Transactional
     public void updateItem(Long id, ItemForm form) {
         Item item = findItem(id);
         item.update(form.getName(), form.getDescription());
         itemRepository.save(item);
     }
 
+    @Transactional
     public void deleteItem(Long id) {
         itemRepository.deleteById(id);
     }
 
+    @Transactional
     public void rentItem(Long id) {
         Item item = findItem(id);
         item.rent();
         itemRepository.save(item);
     }
 
+    @Transactional
     public void returnItem(Long id) {
         Item item = findItem(id);
         item.returnRent();

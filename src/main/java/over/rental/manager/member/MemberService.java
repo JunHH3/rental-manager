b1033ba9af37e600d37fa8cd5e519050bdd03aff
JunHH3 <tarakaing@naver.com>
2026-09-10 10:44:02 +0900

    day23 로그인

--- src/main/java/over/rental/manager/member/MemberService.java
+++ src/main/java/over/rental/manager/member/MemberService.java
@@ -2,6 +2,9 @@ package over.rental.manager.member;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+
 @Service
 public class MemberService {
 
@@ -14,5 +17,17 @@ public class MemberService {
     public Member join(String username, String password) {
         Member member = new Member(username, password);
         return memberRepository.save(member);
+    }
+
+    public Member login(String username, String password) {
+        Optional<Member> member = memberRepository.findByUsername(username);
+        Member foundMember = member.orElseThrow(
+                () -> new NullPointerException("아이디를 찾을 수 없습니다.")
+        );
+
+        if (password.equals(foundMember.getPassword())) {
+            return foundMember;
+        }
+        throw new IllegalArgumentException("비밀번호가 일치하지 않습니다");
     }
 }
--- src/test/java/over/rental/manager/member/MemberServiceTest.java
+++ src/test/java/over/rental/manager/member/MemberServiceTest.java
@@ -5,8 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class MemberServiceTest {
@@ -18,9 +17,29 @@ public class MemberServiceTest {
     @Transactional
     void joinTest(){
         Member savedMember = memberService.join("test", "1234");
+
         assertNotNull(savedMember.getId());
         assertEquals("test", savedMember.getUsername());
         assertEquals("1234", savedMember.getPassword());
+    }
 
+    @Test
+    @Transactional
+    void loginTest(){
+        memberService.join("test", "1234");
+        Member loginMember = memberService.login("test", "1234");
+
+        assertNotNull(loginMember.getId());
+        assertEquals("test", loginMember.getUsername());
+        assertEquals("1234", loginMember.getPassword());
+    }
+
+    @Test
+    @Transactional
+    void loginFailTest(){
+        memberService.join("test", "1234");
+
+        assertThrows(IllegalArgumentException.class,
+                () -> memberService.login("test", "1122"));
     }
 }
